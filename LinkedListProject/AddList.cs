1e65bffe9964e167cceaeaa868f707b14f71c2fe
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-02 08:13:04 +0530

    Add files via upload

---
+++ LinkedListProject/AddList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList1.cs
+{
+    internal class AddList
+    {
+        internal Node head; //new 
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+                Console.WriteLine("The List is empty.\n");
+            while(temp!=null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedListProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using LinkedList1.cs;
+using System;
+namespace LinkedList;
+class SimpleLinkedList
+{
+    public static void Main()
+    {
+        //creating object of class AddList
+        AddList List = new AddList();
+        //calling the Add method via object of the class
+        List.Add(56);
+        List.Add(30);
+        List.Add(70);
+        List.Add(23);
+        //calling the display method via object of the class
+        List.Display();
+    }
+}
