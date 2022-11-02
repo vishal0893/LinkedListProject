8987b836e03f9dce094a2764193d9ac37ecbe842
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-02 08:18:34 +0530

    Add files via upload

---
+++ Linkedlist4.cs/Node4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist4.cs
+{
+    public class Node4
+    {
+        public int data;
+        public Node4 next;
+        public Node4(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Linkedlist4.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using Linkedlist4.cs;
+using System;
+namespace PopLast;
+class Del
+{
+    public static void Main(string[] args)
+    {
+        AddnDelete AnD = new AddnDelete();
+        AnD.Add(30);
+        AnD.Add(56);
+        AnD.Add(70);
+        AnD.Display();
+        //AnD.RemoveFirstNode();
+       // AnD.Display();
+        AnD.Add(98);
+        AnD.Add(78);
+        AnD.Display();
+        //AnD.RemovaLastNode();
+        AnD.Search(70);
+    }
+}
