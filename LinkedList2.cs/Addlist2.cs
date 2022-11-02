d53c82db5d815104fbb47d792b46ae33561e2b75
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-02 08:14:42 +0530

    Add files via upload

---
+++ LinkedList2.cs/Addlist2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList2.cs
+{
+    internal class AddList
+    {
+        internal Node head; //new
+        public AddList()
+        {
+            head = null;
+        }
+        internal void AddFirst(int data)
+        {
+            Node NN = new Node(data);
+            NN.next = head;
+            head = NN;
+            Console.WriteLine("{0} inserted into the linked list", NN.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+                Console.Write("The list is empty.");
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+            }
+
+        }
+    }
+}
---
+++ LinkedList2.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using LinkedList2.cs;
+using System;
+namespace LinkedList2;
+class List2
+{
+    public static void Main()
+    {
+        AddList LL = new AddList();
+        LL.AddFirst(30);
+        LL.AddFirst(56);
+        LL.AddFirst(70);
+        LL.Display(); 
+    
+    }
+}
+
