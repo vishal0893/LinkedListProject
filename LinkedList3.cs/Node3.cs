87d55c397046987b2f1e3c2225327ffc5359eb24
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-11-02 08:16:59 +0530

    Add files via upload

---
+++ LinkedList3.cs/Node3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList3.cs
+{
+    internal class Node3
+    {
+        public int data;
+        public Node3 next;
+        public Node3(int x)
+        {
+            data = x;
+        }
+    }
+}
